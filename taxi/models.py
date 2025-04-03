59f7fa98004c54154d1e571412773e584529950c
Maksym Omelkov <omelkov85@gmail.com>
2025-04-03 23:05:20 +0300

    Taxi service startproject (1.1)

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100)
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=100, unique=True)
+
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        related_name="cars",
+        on_delete=models.CASCADE
+    )
+    model = models.CharField(max_length=100)
+    drivers = models.ManyToManyField(Driver, related_name="cars")
